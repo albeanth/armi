a7dab151c48c423095850ac83af49a6fca487ec1
aalberti <c-aalberti@terrapower.com>
2022-09-08 14:20:03 -0700

    fixes to get the axialExpChngr unittests to pass

--- armi/reactor/converters/axialExpansionChanger.py
+++ armi/reactor/converters/axialExpansionChanger.py
@@ -215,17 +215,17 @@ class AxialExpansionChanger:
             # if ib == 0, leave block bottom = 0.0
             if ib > 0:
                 b.p.zbottom = self.linked.linkedBlocks[b][0].p.ztop
-            # if not in the dummy block, get expansion factor, do alignment, and modify block
-            if ib < (numOfBlocks - 1):
+            isDummyBlock = ib == (numOfBlocks - 1)
+            if not isDummyBlock:
                 ## grow all the components
-                for c in b:
+                for c in _getSolidComponents(b):
                     growFrac = self.expansionData.getExpansionFactor(c)
                     runLog.debug(
                         msg="      Component {0}, growFrac = {1:.4e}".format(
                             c, growFrac
                         )
                     )
-                    if thermal and self.expansionData.componentReferenceHeight:
+                    if thermal and c in self.expansionData.componentReferenceHeight:
                         blockHeight = self.expansionData.componentReferenceHeight[c]
                     if growFrac >= 0.0:
                         c.height = (1.0 + growFrac) * blockHeight
@@ -297,12 +297,7 @@ class AxialExpansionChanger:
                     self.linked.linkedBlocks[b][0].p.ztop != c.zbottom
                     and ib < numOfBlocks - 1
                 ):
-                    if self.linked.linkedBlocks[b][0].hasFlags(
-                        TARGET_FLAGS_IN_PREFERRED_ORDER
-                    ):
-                        c.zbottom = self.linked.linkedBlocks[b][0].p.ztop
-                    else:
-                        self.linked.linkedBlocks[b][0].p.ztop = c.zbottom
+                    b.p.zbottom = self.linked.linkedBlocks[b][0].p.ztop
             if ib == numOfBlocks - 1:
                 b.p.zbottom = self.linked.linkedBlocks[b][0].p.ztop
             elif b.hasFlags(Flags.DUCT) and not adjustedLowestControlDuct:
@@ -503,6 +498,7 @@ def _getComponent(b, componentFlag):
         )
     return comps[0]
 
+
 def _getSolidComponents(b):
     """
     Return list of components in the block that have solid material.
@@ -513,6 +509,7 @@ def _getSolidComponents(b):
     number densities on fluid materials to account for changes in block height.
     """
     return [c for c in b if not isinstance(c.material, material.Fluid)]
+
 
 def _checkBlockHeight(b):
     if b.p.height < 3.0:
